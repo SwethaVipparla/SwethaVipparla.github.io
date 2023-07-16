ec142f607fac13d1b360782bf3de031021a0673c
Swetha Vipparla <swethasreenivas@hotmail.com>
2023-07-16 09:41:37 +0530

    Revamped text

--- js/navbar.js
+++ js/navbar.js
@@ -5,18 +5,18 @@ $(function () {
   });
 });
 
-document.addEventListener("DOMContentLoaded", function(){
-  window.addEventListener('scroll', function() {
-      if (window.scrollY > 0.05) {
-        document.getElementById('mainNavbar').classList.add('fixed-top');
-        navbar_height = document.querySelector('.navbar').offsetHeight;
-        document.body.style.paddingTop = navbar_height + 'px';
-      } else {
-        document.getElementById('mainNavbar').classList.remove('fixed-top');
-        document.body.style.paddingTop = '0';
-      } 
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 0.05) {
+      document.getElementById("mainNavbar").classList.add("fixed-top");
+      navbar_height = document.querySelector(".navbar").offsetHeight;
+      document.body.style.paddingTop = navbar_height + "px";
+    } else {
+      document.getElementById("mainNavbar").classList.remove("fixed-top");
+      document.body.style.paddingTop = "0";
+    }
   });
-}); 
+});
 
 const hover = (link) => {
   link.style.color = "#EA1C2C";
